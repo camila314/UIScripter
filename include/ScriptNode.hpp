79d137f7645bb90c62516383eaf83433441e892c
camila314 <47485054+camila314@users.noreply.github.com>
2025-09-21 11:46:37 -0500

    createDebugNode

--- include/ScriptNode.hpp
+++ include/ScriptNode.hpp
@@ -376,6 +376,31 @@ namespace uiscripter {
 			}
 		}
 	};
+
+	void createDebugNode(CCNode* parent, char const* parentfile, char const* filename) {
+		auto file = std::filesystem::path(parentfile).parent_path() / filename;
+
+		Build(parent).schedule([parent, file](float dt) {
+			if (!std::filesystem::exists(file))
+				return;
+
+			auto last_write = std::filesystem::last_write_time(file);
+			static decltype(last_write) last_check;
+
+			if (last_check != last_write) {
+				last_check = last_write;
+
+				std::string code = utils::file::readString(file).unwrap();
+
+				if (auto node = uiscripter::ScriptNode::create(code)) {
+					parent->removeChildByID("script-node");
+					node->setID("script-node");
+					node->setZOrder(99999);
+					Build(node).parent(parent).center();
+				}
+			}
+		});
+	}
 }
 
 using uiscripter::ScriptNode;
